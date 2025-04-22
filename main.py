a07cbf784216d434c68ac749393bcb3bdc9c0cae
ilmir <adm.eths@gmail.com>
2025-04-22 21:19:33 +0500

    Правки по коду Добавлен Enum

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from enum import Enum
 
 
-def main():
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = RotatingFileHandler(
+        "my.log",
+        mode="a",
+        maxBytes=1 * 1024 * 1024,
+        backupCount=2,
+        encoding="UTF-8",
+    )
+    logger.addHandler(file_handler)
+
+    logger_formatter = logging.Formatter(
+        "[%(asctime)s.%(msecs)03d] %(module)-15s:%(lineno)-3d %(levelname)-7s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    file_handler.setFormatter(logger_formatter)
+
+
     print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
-
     board = list(range(1, 10))
 
+
     def draw_board(board):
-        try:
+        print("-" * 13)
+        for i in range(3):
+            print(
+                "|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|"
+            )
             print("-" * 13)
-            for i in range(3):
-                print(
-                    "|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|"
-                )
-                print("-" * 13)
-            logging.info("Доска обновлена")
-        except Exception as err:
-            logger.warning(err)
+        logger.info("Доска обновлена")
+
+
 
     def take_input(player_token):
         valid = False
@@ -25,8 +44,8 @@ def main():
             player_answer = input("Куда поставим " + player_token + "? ")
             try:
                 player_answer = int(player_answer)
-                logger.info("Пользователь поставил %s в %s",player_token,player_answer)
-            except Exception as err:
+                logger.info("Пользователь поставил %s в %s", player_token, player_answer)
+            except ValueError as err:
                 print("Некорректный ввод. Вы уверены, что ввели число?")
                 logger.info(err)
                 continue
@@ -36,7 +55,7 @@ def main():
                     valid = True
                 else:
                     print("Эта клетка уже занята!")
-                    logger.info("Попытка поставить в занятую клетку %s",player_answer)
+                    logger.info("Попытка поставить в занятую клетку %s", player_answer)
             else:
                 print("Некорректный ввод. Введите число от 1 до 9.")
 
@@ -57,15 +76,22 @@ def main():
                 return board[each[0]]
         return False
 
+
     def main(board):
         counter = 0
         win = False
+
+        class Token(Enum):
+            X = "X"
+            O = "O"
+
+
         while not win:
             draw_board(board)
             if counter % 2 == 0:
-                take_input("X")
+                take_input(Token.X.value)
             else:
-                take_input("O")
+                take_input(Token.O.value)
             counter += 1
             if counter > 4:
                 tmp = check_win(board)
@@ -78,29 +104,8 @@ def main():
                 break
         draw_board(board)
 
+
     main(board)
 
     input("Нажмите Enter для выхода!")
 
-
-if __name__ == "__main__":
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-
-    file_handler = RotatingFileHandler(
-        "my.log",
-        mode="a",
-        maxBytes=1 * 1024 * 1024,
-        backupCount=2,
-        encoding="UTF-8",
-    )
-
-    logger.addHandler(file_handler)
-
-    logger_formatter = logging.Formatter(
-        "[%(asctime)s.%(msecs)03d] %(module)-15s:%(lineno)-3d %(levelname)-7s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    file_handler.setFormatter(logger_formatter)
-
-    main()
